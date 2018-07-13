d203828838491d190a4d687e04661b5bb888842c
Akhilgupta17 <akhilgupta1711@gmail.com>
2018-07-13 10:01:05 +0530

    Api

---
+++ 1.py
@@ -0,0 +1,7 @@
+Q.What Is An Access token?
+Ans.An access token is an object that describes the security context of a process or thread. 
+    The information in a token includes the identity and privileges of the user account associated with the process or thread.
+    When a user logs on, the system verifies the user's password by comparing it with information stored in a security database. 
+    If the password is authenticated, the system produces an access token. Every process executed on behalf of this user has a copy of this access token.
+
+MY Twiiter App Access Token:     Access Token :	782466154728075264-Pg2trbePRUvRIlIWtaeKZAcNhMELIll
